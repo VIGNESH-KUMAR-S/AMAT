59d51fd1b40790ea7ad81fd79c9ec9efd5c69232
Vignesh Kumar S <vigneshkumar.ec17@bitsathy.ac.in>
2021-08-16 13:59:13 +0530

    Add priority_queue source code

--- Data Structures/Queue/queue.cpp
+++ Data Structures/Queue/queue.cpp
@@ -21,4 +21,16 @@ int main(void)
 	}
 
 	// All other functions like swap(), emplace() are worked same like stack.
+
+	// PRIORITY QUEUE
+	priority_queue<int> pq;
+	pq.push(12);	pq.push(23);	pq.push(9);		pq.push(99);	pq.push(6);
+	// Here though we give multiple unordered values it'll be automatically sorted in descending order.
+
+	cout << "\n\nPRIORITY QUEUE\nElements : ";
+	while (!pq.empty())
+	{
+		cout << pq.top() << "\t";
+		pq.pop();
+	}
 }
